ed11718d9999ec484651ee1790b883137269df6d
advancingKoala <milaiduoduo4567@163.com>
2018-04-11 19:07:13 +0800

    注释另一种home组件使用方式，也可以用注册组件的方式使用home组件

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
 <template>
   <div id="app">
     <router-view></router-view>
+
+    <!--也可以用注册组件的方式使用home组件-->
+    <!--<home></home>-->
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+//  也可以用注册组件的方式使用home组件
+//  import home from './components/home';
+//  export default {
+//    name: 'App',
+//    components: {
+//      home
+//    }
+//  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
