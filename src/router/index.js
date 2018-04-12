dbd0273d5b4a60fb6cae7643ec7aceabcc6b34fe
advancingKoala <milaiduoduo4567@163.com>
2018-04-12 19:27:19 +0800

    在功能上实现对我的项目和我的工作台需要登录后才能进入页面，如果未登录则进入login页面。采用路由拦截器的方式来实现该功能。在全局使用router.beforeEach方法在作为拦截器。

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from '@/components/home';
-import Layout from '@/components/layoutbackend';
-import Doc from '@/views/doc';
-import Project from '@/views/project';
-import Workbench from '@/views/workbench';
-import Login from '@/views/login';
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/home'
+import Layout from '@/components/layoutbackend'
+import Doc from '@/views/doc'
+import Project from '@/views/project'
+import Workbench from '@/views/workbench'
+import Login from '@/views/login'
 
 Vue.use(Router)
 
@@ -28,23 +28,52 @@ let router = new Router({
       component: Layout,
       children: [
         {
-          path: '/doc',
-          name: 'Doc',
-          component: Doc
-        },
-        {
           path: '/project',
           name: 'Project',
-          component: Project
+          component: Project,
+          meta: {
+            needLogin: true
+          }
         },
         {
           path: '/workbench',
           name: 'Workbench',
-          component: Workbench
+          component: Workbench,
+          meta: {
+            needLogin: true
+          }
+        },
+        {
+          path: '/doc',
+          name: 'Doc',
+          component: Doc,
+          meta: {
+            needLogin: false
+          }
         }
       ]
     }
   ]
+})
+
+// 相当于路由拦截器的功能
+// 实现功能：拦截必须登录才能看的url，经过判断登录后的才能进入，未登录则跳到登录页。
+router.beforeEach((to, from, next) => {
+  // to.matched 取到的是url匹配出来的组件和它的父组件obj
+  let flag = to.matched.some((item) => item.meta.needLogin)
+  console.log('to: ', to)
+  console.log('是否登录后才能使用：', flag)
+  if (flag) {
+    // router.app 取到的是当前vue实例
+    let info = router.app.$local_sf.fetch('vueRouter_p1')
+    if (info.login) {
+      next()
+    } else {
+      router.push({path: '/login'})
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
